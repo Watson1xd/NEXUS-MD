362bd08d09eee8b88ecec7741972918e3ac38187
Watson1xd <fourpencewatson37@gmail.com>
2024-10-17 08:01:03 +0200

    Create ai.js

---
+++ plugins/ai.js
@@ -0,0 +1,34 @@
+/*
+
+THIS CODE WAS WRITTEN BY MR Chami ( Chami ) - ( FREELY ).
+    - @Chamindu
+GITHUB : https://github.com/dark-chami21
+------------------------------------------------------------------
+CODE FOR : @dark-chami21
+------------------------------------------------------------------
+DON'T REMOVE CREDITS !
+
+*/
+
+
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "ai",
+    react: "🤖",
+    desc: "ai chat",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+return reply(`${data.data}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
